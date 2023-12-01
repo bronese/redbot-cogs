e34681d4253f3779d492affb7c0d8504157bbf7e
bronese <nghanwei99@live.com>
2023-11-30 23:51:35 -0500

    modified:   vxtwitter/vxtwitter.py

--- vxtwitter/vxtwitter.py
+++ vxtwitter/vxtwitter.py
@@ -23,7 +23,7 @@ class vxtwitter(commands.Cog):
                     if webhook is None:
                         webhook = await message.channel.create_webhook(name="vxtwitter")
                     try:
-                        await webhook.send(new_content, username=message.author.nickname, avatar_url=message.author.avatar)
+                        await webhook.send(new_content, username=message.author.display_name, avatar_url=message.author.avatar)
                         await message.delete()  # delete the original message
                     except Exception as e:
                        await message.channel.send(f"Failed to send message: {e}")

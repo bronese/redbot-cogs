93c9676f22eeb2fe2919b5d8eb156306b07b9b72
bronese <nghanwei99@live.com>
2023-11-30 23:14:10 -0500

    modified:   vxtwitter/vxtwitter.py

--- vxtwitter/vxtwitter.py
+++ vxtwitter/vxtwitter.py
@@ -11,21 +11,21 @@ class vxtwitter(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
-            await message.channel.send("bot ignored")
             return
         if any(url.startswith(("https://twitter.com", "https://x.com")) for url in message.content.split()):   
-            await message.channel.send("triggered 2")
             new_content = message.content
             for url in message.content.split():
                 if url.startswith("twitter.com") or url.startswith("x.com"):
                     new_url = url.replace(url.split(".com")[0], "vxtwitter")
                     new_content = new_content.replace(url, new_url)
+                    await message.channel.send(new_content)
             # Use the webhook to send the message
             webhooks = await message.channel.webhooks()
             if webhooks:
                 webhook = webhooks[0]
             else:
                 webhook = await message.channel.create_webhook(name="vxtwitter")
+                await message.channel.send("webhook created")
             async with aiohttp.ClientSession() as session:
                 webhook = discord.Webhook.from_url(webhook.url, adapter=discord.AsyncWebhookAdapter(session))
                 await webhook.send(new_content, username=message.author.name, avatar_url=message.author.avatar_url)

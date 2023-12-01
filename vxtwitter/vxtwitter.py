5e11ab636ebb33930557a32919e2596709d89cc4
bronese <nghanwei99@live.com>
2023-11-30 23:36:32 -0500

    modified:   vxtwitter/vxtwitter.py

--- vxtwitter/vxtwitter.py
+++ vxtwitter/vxtwitter.py
@@ -25,7 +25,7 @@ class vxtwitter(commands.Cog):
                         webhook = await message.channel.create_webhook(name="vxtwitter")
                         await message.channel.send("webhook created")
                     try:
-                        await webhook.send(new_content, username=message.author.name, avatar_url=message.author.avatar_url)
+                        await webhook.send(new_content, username=message.author.name, avatar_url=message.author.avatar)
                     except Exception as e:
                        await message.channel.send(f"Failed to send message: {e}")
 def setup(bot):

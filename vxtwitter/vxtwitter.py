a2b73f3634628f8571011886658484d08950f75c
bronese <nghanwei99@live.com>
2023-11-30 21:45:53 -0500

    initial

---
+++ vxtwitter/vxtwitter.py
@@ -0,0 +1,21 @@
+from redbot.core import commands
+
+class MyCog(commands.Cog):
+    """My custom cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if any(url.startswith(("twitter.com", "x.com")) for url in message.content.split()):
+            new_content = message.content
+            for url in message.content.split():
+                if url.startswith("twitter.com") or url.startswith("x.com"):
+                    new_url = url.replace(url.split(".com")[0], "vxtwitter")
+                    new_content = new_content.replace(url, new_url)
+
+            await message.channel.send(new_content, username=message.author.name, avatar_url=message.author.avatar_url)

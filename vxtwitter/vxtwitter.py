194c3d1ee3dcdbb9b77952ec99813c400c5d4948
bronese <nghanwei99@live.com>
2023-11-30 22:49:12 -0500

    modified:   vxtwitter/requirements.txt 	modified:   vxtwitter/vxtwitter.py

--- vxtwitter/vxtwitter.py
+++ vxtwitter/vxtwitter.py
@@ -1,4 +1,6 @@
 from redbot.core import commands
+import discord
+import aiohttp
 
 class vxtwitter(commands.Cog):
     """replaces vxtwitter"""
@@ -11,16 +13,20 @@ class vxtwitter(commands.Cog):
         if message.author.bot:
             return
         if any(url.startswith(("twitter.com", "x.com")) for url in message.content.split()):
-            await message.channel.send("hi")
             new_content = message.content
             for url in message.content.split():
                 if url.startswith("twitter.com") or url.startswith("x.com"):
                     new_url = url.replace(url.split(".com")[0], "vxtwitter")
                     new_content = new_content.replace(url, new_url)
-            # Use the channel object to send the message
-            await message.channel.send(
-                str(new_content), username=message.author.name, avatar_url=message.author.avatar_url
-            )
+            # Use the webhook to send the message
+            webhooks = await message.channel.webhooks()
+            if webhooks:
+                webhook = webhooks[0]
+            else:
+                webhook = await message.channel.create_webhook(name="vxtwitter")
+            async with aiohttp.ClientSession() as session:
+                webhook = discord.Webhook.from_url(webhook.url, adapter=discord.AsyncWebhookAdapter(session))
+                await webhook.send(new_content, username=message.author.name, avatar_url=message.author.avatar_url)
 
 def setup(bot):
     bot.add_cog(vxtwitter(bot))
